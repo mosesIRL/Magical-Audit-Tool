ad2b0c39fab1f1e2202eee007f2e747376eded95
Mathew Giljum <mathew.giljum@gmail.com>
2017-03-14 15:54:59 -0400

    Cleanup

--- MAT.bat
+++ MAT.bat
@@ -1,71 +1,56 @@
 REM https://github.com/mgiljum/MAT
 @echo off
 mode con: cols=100 lines=35
-echo " ___  ___  ___  ______  __     ___  ___  ___    ___  __   ___  ___  __      
-echo " ||\\//|| // \\ | || | (( \    ||\\//|| // \\  // \\ ||  //   // \\ ||      
-echo " || \/ || ||=||   ||    \\     || \/ || ||=|| (( ___ || ((    ||=|| ||      
-echo " ||    || || ||   ||   \_))    ||    || || ||  \\_|| ||  \\__ || || ||__|   
-echo "                                                                            
-echo "  ___  __ __ ____   __ ______    ______   ___     ___   __                  
-echo " // \\ || || || \\  || | || |    | || |  // \\   // \\  ||                  
-echo " ||=|| || || ||  )) ||   ||        ||   ((   )) ((   )) ||                  
-echo " || || \\_// ||_//  ||   ||        ||    \\_//   \\_//  ||__|    (or "MAT" for short)           
-echo "                                                                            
+
+
+:INTRO
+type "%~dp0inc\header.txt"
 echo.
 echo.
-echo This tool grabs the user's name, PC name, Windows version, and a list of 
-echo any Microsoft software (with version numbers) and plops it in a .csv file.
-echo.
-echo ONLY WORKS WITH WINDOWS VISTA / WINDOWS SERVER 2008 AND UP 
-echo (WILL NOT WORK ON SERVER 2003 / XP OR BELOW)
-echo.
-echo.
-echo If an audit CSV for this computer (or one with the same name) has already been created,
-echo running the tool again will overwrite it.
-echo.
-echo.
-PAUSE
+type "%~dp0inc\intro.txt"
+PAUSE >NUL
 cls
-mode con: cols=100 lines=35
-echo " ___  ___  ___  ______  __     ___  ___  ___    ___  __   ___  ___  __      
-echo " ||\\//|| // \\ | || | (( \    ||\\//|| // \\  // \\ ||  //   // \\ ||      
-echo " || \/ || ||=||   ||    \\     || \/ || ||=|| (( ___ || ((    ||=|| ||      
-echo " ||    || || ||   ||   \_))    ||    || || ||  \\_|| ||  \\__ || || ||__|   
-echo "                                                                            
-echo "  ___  __ __ ____   __ ______    ______   ___     ___   __                  
-echo " // \\ || || || \\  || | || |    | || |  // \\   // \\  ||                  
-echo " ||=|| || || ||  )) ||   ||        ||   ((   )) ((   )) ||                  
-echo " || || \\_// ||_//  ||   ||        ||    \\_//   \\_//  ||__|               
-echo "                                                                            
+
+
+:START
+type "%~dp0inc\header.txt"
 echo.
 echo.
 echo Please wait, collecting data...
 echo.
 echo (This process may take up to 10 minutes on servers)
-del /q "%~dp0%ComputerName%-Audit.csv" 2>nul
+REM Create results directory and delete identical results
+if not exist "%~dp0results" (
+	mkdir "%~dp0results"
+	)
+if exist "%~dp0results\%ComputerName%-Audit.csv" (
+	del /q "%~dp0results\%ComputerName%-Audit.csv"
+	)
+
+REM Get Windows version
 FOR /F "tokens=2*" %%A IN ('REG.EXE QUERY "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /V "ProductName" 2^>NUL ^| Find "REG_SZ"') Do Set RegValuedata=%%B
-echo User,PC Name,Windows Version >> "%~dp0%ComputerName%-Audit.csv"
-echo %userdomain%\%username%,%ComputerName%,%RegValuedata%>> "%~dp0%ComputerName%-Audit.csv"
-echo. >> "%~dp0%ComputerName%-Audit.csv"
-echo. >> "%~dp0%ComputerName%-Audit.csv"
-echo SOFTWARE >> "%~dp0%ComputerName%-Audit.csv"
+
+REM Echo out User,PC Name,Windows Version to results
+echo User,PC Name,Windows Version >> "%~dp0results\%ComputerName%-Audit.csv"
+echo %userdomain%\%username%,%ComputerName%,%RegValuedata%>> "%~dp0results\%ComputerName%-Audit.csv"
+
+REM Add some blank lines
+echo. >> "%~dp0results\%ComputerName%-Audit.csv"
+echo. >> "%~dp0results\%ComputerName%-Audit.csv"
+
+REM Grab software and echo to results
+echo SOFTWARE >> "%~dp0results\%ComputerName%-Audit.csv"
 setlocal enabledelayedexpansion
 (For /F "tokens=2,3 delims=," %%A in ('"wmic product where "Vendor like '%%Microsoft%%'" get Name,Version /format:csv"') do (
 set "line=%%A,%%B"
 echo !line:~0,-1!
-))>>"%~dp0%ComputerName%-Audit.csv"
+))>>"%~dp0results\%ComputerName%-Audit.csv"
 cls
-mode con: cols=100 lines=35
-echo " ___  ___  ___  ______  __     ___  ___  ___    ___  __   ___  ___  __      
-echo " ||\\//|| // \\ | || | (( \    ||\\//|| // \\  // \\ ||  //   // \\ ||      
-echo " || \/ || ||=||   ||    \\     || \/ || ||=|| (( ___ || ((    ||=|| ||      
-echo " ||    || || ||   ||   \_))    ||    || || ||  \\_|| ||  \\__ || || ||__|   
-echo "                                                                            
-echo "  ___  __ __ ____   __ ______    ______   ___     ___   __                  
-echo " // \\ || || || \\  || | || |    | || |  // \\   // \\  ||                  
-echo " ||=|| || || ||  )) ||   ||        ||   ((   )) ((   )) ||                  
-echo " || || \\_// ||_//  ||   ||        ||    \\_//   \\_//  ||__|               
-echo "                                                                            
+
+
+:FINISHED
+type "%~dp0inc\header.txt"
+echo.
 echo.
 echo Success^^!
 echo.
