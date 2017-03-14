dc8a5549654a2347e3e92cb154fb370d9742dbe4
Mathew Giljum <mathew.giljum@gmail.com>
2017-03-14 16:16:22 -0400

    wording, format

--- MAT.bat
+++ MAT.bat
@@ -18,7 +18,7 @@ echo.
 echo.
 echo Please wait, collecting data...
 echo.
-echo (This process may take up to 10 minutes on servers)
+echo (This process may take up to 10 minutes on some devices)
 
 REM CREATE RESULTS DIR / DEL EXISTING
 if not exist "%~dp0results" (
@@ -47,10 +47,9 @@ echo SOFTWARE >> "%~dp0results\%ComputerName%-Audit.csv"
 
 REM SECTION2 DATA
 setlocal enabledelayedexpansion
-(For /F "tokens=2,3 delims=," %%A in ('"wmic product where "Vendor like '%%Microsoft%%'" get Name,Version /format:csv"') do (
-set "line=%%A,%%B"
-echo !line:~0,-1!
-))>>"%~dp0results\%ComputerName%-Audit.csv"
+(for /F "tokens=2,3 delims=," %%A in ('"wmic product where "Vendor like '%%Microsoft%%'" get Name,Version /format:csv"') do (
+	set "line=%%A,%%B"
+	echo !line:~0,-1!)) >>"%~dp0results\%ComputerName%-Audit.csv"
 cls
 
 
